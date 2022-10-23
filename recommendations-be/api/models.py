197fd5a6d41b55dcd62aa92e1e9bc583e7d78b21
Mateusz Jabłoński <mateusz.jablonski12@gmail.com>
2022-10-23 11:08:16 +0200

    Add "Recommender" model

--- recommendations-be/api/models.py
+++ recommendations-be/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Recommender(models.Model):
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField()
+    current_points = models.IntegerField()
+    credibility = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
