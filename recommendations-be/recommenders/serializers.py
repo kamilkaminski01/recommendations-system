1d0b87d2b2bf97bbe1bdec0c5aabfbb1eb94caa2
Kamil Kaminski <kamilkaminski39@gmail.com>
2022-11-27 17:40:31 +0100

    UR-43 fixed recommenders endpoints

--- recommendations-be/recommenders/serializers.py
+++ recommendations-be/recommenders/serializers.py
@@ -1,9 +1,54 @@
+from django.contrib.auth import password_validation
 from rest_framework import serializers
 
 from .models import Recommender
 
 
 class RecommenderSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
     class Meta:
         model = Recommender
-        fields = "__all__"
+        fields = ["email", "first_name", "last_name", "password"]
+
+    def create(self, validated_data: dict) -> Recommender:
+        recommender = super().create(validated_data)
+        recommender.set_password(validated_data["password"])
+        recommender.save()
+        return recommender
+
+    def validate_password(self, data: str) -> str:
+        password_validation.validate_password(data, self.instance)
+        return data
+
+
+class RecommenderDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommender
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "address",
+            "current_points",
+            "credibility",
+        ]
+
+
+class RecommenderUpdateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = Recommender
+        fields = ["email", "first_name", "last_name", "address", "password"]
+        read_only_fields = ["email"]
+
+    def update(self, instance, validated_data: dict) -> Recommender:
+        recommender = super().update(validated_data)
+        recommender.set_password(validated_data["password"])
+        recommender.save()
+        return recommender
+
+    def validate_password(self, data: str) -> str:
+        password_validation.validate_password(data, self.instance)
+        return data
--- recommendations-be/recommenders/views.py
+++ recommendations-be/recommenders/views.py
@@ -1,12 +1,32 @@
 from rest_framework import generics, mixins
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
 
 from .models import Recommender
-from .serializers import RecommenderSerializer
+from .serializers import (
+    RecommenderDetailsSerializer,
+    RecommenderSerializer,
+    RecommenderUpdateSerializer,
+)
 
 
-class RecommendersAPIView(
+class RecommenderListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Recommender.objects.all()
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def perform_create(self, serializer: RecommenderSerializer):
+        serializer.save()
+
+    def get_serializer_class(self) -> ModelSerializer:
+        if self.request.method in ["POST"]:
+            return RecommenderSerializer
+        return RecommenderDetailsSerializer
+
+
+class RecommenderDetailsAPIView(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
     mixins.CreateModelMixin,
@@ -14,24 +34,25 @@ class RecommendersAPIView(
     generics.GenericAPIView,
 ):
     queryset = Recommender.objects.all()
-    serializer_class = RecommenderSerializer
+    serializer_class = RecommenderDetailsSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request: Request, *args, **kwargs) -> Response:
+        return self.destroy(request, *args, **kwargs)
+
+
+class RecommenderUpdateAPIView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Recommender.objects.all()
+    serializer_class = RecommenderUpdateSerializer
 
     def get(self, request: Request, *args, **kwargs) -> Response:
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request: Request, *args, **kwargs) -> Response:
         return self.update(request, *args, **kwargs)
-
-    def post(self, request: Request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
-
-    def delete(self, request: Request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
-
-
-class RecommenderListAPIView(mixins.ListModelMixin, generics.GenericAPIView):
-    queryset = Recommender.objects.all()
-    serializer_class = RecommenderSerializer
-
-    def get(self, request: Request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
