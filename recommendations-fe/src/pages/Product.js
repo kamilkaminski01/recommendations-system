1dfd8cd3ba0f48fb4130620b66a0e4bb968852ed
Mateusz <mateusz.jablonski12@gmail.com>
2023-01-21 22:50:57 +0100

    UR-59 Add fetch by id and layout

--- recommendations-fe/src/pages/Product.js
+++ recommendations-fe/src/pages/Product.js
@@ -1,24 +1,44 @@
 
 import logo from "./face.jpg";
+import { useParams } from "react-router-dom";
+import * as React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
+export default function Product(props) {
 
+    const {userId} = useParams();
+    
+    const API_URL = "http://localhost:8000/api/";
+    const [dataProvider, setDataProvider] = React.useState([]);
+    const [isLoading,SetLoading] = React.useState(true);
 
-export default function Product() {
+    useEffect(() => {
+        axios.get(API_URL + "shop/rewards/"+userId).then((response) => {
+        setDataProvider(response.data);
+        SetLoading(false);
+        });
+    }, []);
 
+    console.log(dataProvider);
+    if(isLoading){
+        return (<><CircularProgress/></>);
+    }
 
     return(
         <>
     <section class="py-5">
     <div class="container product_data px-4 px-lg-5 my-5">
         <div class="row gx-4 gx-lg-5 align-items-center">
-            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0"  src={require("./face.jpg")} alt="<?= $product['image'] ?>" /></div>
+            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0"  src={dataProvider.reward_images[0].image} /></div>
             <div class="col-md-6">
                 <div class="small mb-1"></div>
-                <h1 class="display-5 fw-bolder">Pan ze stocka</h1>
+                <h1 class="display-5 fw-bolder">{dataProvider.title}</h1>
                 <div class="fs-5 mb-5">
-                    <span>200$</span>
+                    <span>{dataProvider.cost}$</span>
                 </div>
-                <p class="lead">Tutaj bedzie opis porduktu narazie pan ze stocka</p>
+                <p class="lead">{dataProvider.long_description}</p>
 
                 <div class="d-flex">
                          
