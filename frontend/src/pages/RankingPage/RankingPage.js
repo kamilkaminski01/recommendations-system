1c4d2caad3d5e1f49ed967747d05b5f566afdd73
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-06-12 12:18:04 +0200

    Fixed empty users in ranking page

--- frontend/src/pages/RankingPage/RankingPage.js
+++ frontend/src/pages/RankingPage/RankingPage.js
@@ -35,17 +35,20 @@ export default function RankingPage() {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {dataProvider.map((dataProvider) => (
-                  <TableRow
-                    key={dataProvider.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell component="th" scope="row">
-                      {dataProvider.first_name}
-                    </TableCell>
-                    <TableCell>{dataProvider.last_name}</TableCell>
-                    <TableCell>{dataProvider.credibility}</TableCell>
-                  </TableRow>
-                ))}
+                {dataProvider.map(
+                  (dataProvider) =>
+                    dataProvider.first_name && (
+                      <TableRow
+                        key={dataProvider.id}
+                        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                        <TableCell component="th" scope="row">
+                          {dataProvider.first_name}
+                        </TableCell>
+                        <TableCell>{dataProvider.last_name}</TableCell>
+                        <TableCell>{dataProvider.credibility}</TableCell>
+                      </TableRow>
+                    ),
+                )}
               </TableBody>
             </Table>
           </TableContainer>
